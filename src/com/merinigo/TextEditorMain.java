228729176391b135768df7f6c2d738ed6c230ff3
Iñigo <merinigo@gmail.com>
2017-02-03 17:07:22 +0100

    Añadido menus especificos de editor de texto
    -Menus añadidos:
        -Menu Edicion
        -Menu Opciones
    
    -Botones Toolbar añadidos:
        -Cortar
        -Copiar
        -Pegar
    
    Otros cambios:
        -Modificacion de iconos

--- src/com/merinigo/TextEditorMain.java
+++ src/com/merinigo/TextEditorMain.java
@@ -4,12 +4,6 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Insets;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.ClipboardOwner;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
@@ -18,6 +12,7 @@ import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -31,30 +26,44 @@ import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.border.EtchedBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class TextEditorMain implements ClipboardOwner{
+public class TextEditorMain{
 
 	private JFrame frame;
 	private JMenuBar jmbarBarraDeMenus;
 	private JMenu jmnuArchivo;
-	private JMenuItem jmItemAbrir;
-	private JMenu jmnuGuardar;
 	private JToolBar jtbarBarraDeHerr;
 	private JMenuItem jmItemSalir;
-	private JMenuItem jmItemMismoNombre;
-	private JMenuItem jmItemOtroNombre;
 	private JButton jbtbarAbrir;
 	private JButton jbtbarGuardar;
 	private JPanel jBarraDeEstado;
 	private JLabel jetbarestPpal;
 	private JScrollPane jscrpaneEditor;
 	private JTextArea jtxtaEditor;
-	private Clipboard portapapeles;
-	
+	private JMenu jmnuEdicion;
+	private JMenu jmnuOpciones;
+	private JMenuItem jmItemCopiar;
+	private JMenuItem jmItemPegar;
+	private JMenuItem jmItemCortar;
+	private JMenu jmnuFuente;
+	private JMenuItem jmItemArial;
+	private JMenuItem jmItemFPredeterminada;
+	private JMenuItem jmItemCourierNew;
+	private JMenu jmnuTamaño;
+	private JMenuItem jmItem24;
+	private JMenuItem jmItemTPredeterminado;
+	private JMenuItem jmItem16;
+	private JButton jbtbarCortar;
+	private JButton jbtbarCopiar;
+	private JButton jbtnbarPegar;
+	private JSeparator separator;
+	private JMenuItem jmItemGuardar;
+	private JMenuItem jmItemAbrir;
 
 	/**
 	 * Launch the application.
@@ -81,8 +90,7 @@ public class TextEditorMain implements ClipboardOwner{
 	 * Create the application.
 	 */
 	public TextEditorMain() {
-		initialize();
-		portapapeles = Toolkit.getDefaultToolkit().getSystemClipboard();
+		initialize();		
 	}
 
 	/**
@@ -108,24 +116,75 @@ public class TextEditorMain implements ClipboardOwner{
 		frame.getContentPane().setLayout(null);
 		
 		jtbarBarraDeHerr = new JToolBar();
+		jtbarBarraDeHerr.setFloatable(false);
 		jtbarBarraDeHerr.setBounds(0, 0, 484, 20);
 		frame.getContentPane().add(jtbarBarraDeHerr);
 		
-		jbtbarAbrir = new JButton("");
+		jbtbarAbrir = new JButton("");		
+		jbtbarAbrir.addChangeListener(toolBarItemChangeListener);
 		jbtbarAbrir.setFocusable(false);
 		jbtbarAbrir.setFocusPainted(false);
-		jbtbarAbrir.setMargin(new Insets(2, 2, 2, 0));
+		jbtbarAbrir.setMargin(new Insets(0, 0, 0, 0));
 		jbtbarAbrir.setToolTipText("Abrir");
-		jbtbarAbrir.setIcon(new ImageIcon(TextEditorMain.class.getResource("/icons/Abrir.png")));
+		jbtbarAbrir.setIcon(new ImageIcon(TextEditorMain.class.getResource("/icons/Open.png")));
 		jtbarBarraDeHerr.add(jbtbarAbrir);
 		
 		jbtbarGuardar = new JButton("");
+		jbtbarGuardar.addChangeListener(toolBarItemChangeListener);
+		jbtbarGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		jbtbarGuardar.setFocusable(false);
 		jbtbarGuardar.setFocusPainted(false);
 		jbtbarGuardar.setToolTipText("Guardar");
-		jbtbarGuardar.setMargin(new Insets(2, 2, 2, 0));
-		jbtbarGuardar.setIcon(new ImageIcon(TextEditorMain.class.getResource("/icons/Guardar.png")));
+		jbtbarGuardar.setMargin(new Insets(0, 0, 0, 0));
+		jbtbarGuardar.setIcon(new ImageIcon(TextEditorMain.class.getResource("/icons/Save.png")));
 		jtbarBarraDeHerr.add(jbtbarGuardar);
+		
+		separator = new JSeparator();
+		separator.setMaximumSize(new Dimension(5, 32767));
+		separator.setOrientation(SwingConstants.VERTICAL);
+		jtbarBarraDeHerr.add(separator);
+		
+		jbtbarCortar = new JButton("");
+		jbtbarCortar.addChangeListener(toolBarItemChangeListener);
+		jbtbarCortar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		jbtbarCortar.setIcon(new ImageIcon(TextEditorMain.class.getResource("/icons/Cut.png")));
+		jbtbarCortar.setToolTipText("Cortar");
+		jbtbarCortar.setMargin(new Insets(0, 0, 0, 0));
+		jbtbarCortar.setFocusable(false);
+		jbtbarCortar.setFocusPainted(false);
+		jtbarBarraDeHerr.add(jbtbarCortar);
+		
+		jbtbarCopiar = new JButton("");
+		jbtbarCopiar.addChangeListener(toolBarItemChangeListener);
+		jbtbarCopiar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		jbtbarCopiar.setIcon(new ImageIcon(TextEditorMain.class.getResource("/icons/Copy.png")));
+		jbtbarCopiar.setToolTipText("Copiar");
+		jbtbarCopiar.setMargin(new Insets(0, 0, 0, 0));
+		jbtbarCopiar.setFocusable(false);
+		jbtbarCopiar.setFocusPainted(false);
+		jtbarBarraDeHerr.add(jbtbarCopiar);
+		
+		jbtnbarPegar = new JButton("");
+		jbtnbarPegar.addChangeListener(toolBarItemChangeListener);
+		jbtnbarPegar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		jbtnbarPegar.setIcon(new ImageIcon(TextEditorMain.class.getResource("/icons/Paste.png")));
+		jbtnbarPegar.setToolTipText("Pegar");
+		jbtnbarPegar.setMargin(new Insets(0, 0, 0, 0));
+		jbtnbarPegar.setFocusable(false);
+		jbtnbarPegar.setFocusPainted(false);
+		jtbarBarraDeHerr.add(jbtnbarPegar);
 		
 		jBarraDeEstado = new JPanel();
 		FlowLayout fl_jBarraDeEstado = (FlowLayout) jBarraDeEstado.getLayout();
@@ -154,26 +213,6 @@ public class TextEditorMain implements ClipboardOwner{
 		jmnuArchivo.setMnemonic('A');
 		jmbarBarraDeMenus.add(jmnuArchivo);
 		
-		jmItemAbrir = new JMenuItem("Abrir...");
-		jmItemAbrir.addChangeListener(menuItemChangeListener);
-		jmItemAbrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_MASK));
-		jmItemAbrir.setMnemonic('A');
-		jmnuArchivo.add(jmItemAbrir);
-		
-		jmnuGuardar = new JMenu("Guardar");
-		jmnuGuardar.addChangeListener(menuItemChangeListener);
-		jmnuGuardar.setMnemonic('G');
-		jmnuArchivo.add(jmnuGuardar);
-		
-		jmItemMismoNombre = new JMenuItem("Mismo nombre");
-		jmnuGuardar.add(jmItemMismoNombre);
-		
-		jmItemOtroNombre = new JMenuItem("Otro nombre");
-		jmnuGuardar.add(jmItemOtroNombre);
-		
-		JSeparator jSeparador1 = new JSeparator();
-		jmnuArchivo.add(jSeparador1);
-		
 		jmItemSalir = new JMenuItem("Salir");
 		jmItemSalir.addChangeListener(menuItemChangeListener);	
 		jmItemSalir.setMnemonic('S');
@@ -182,7 +221,59 @@ public class TextEditorMain implements ClipboardOwner{
 				jmItemSalirActionPerformed(evt);
 			}
 		});
+		
+		jmItemAbrir = new JMenuItem("Abrir");
+		jmnuArchivo.add(jmItemAbrir);
+		
+		jmItemGuardar = new JMenuItem("Guardar");
+		jmItemGuardar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		jmnuArchivo.add(jmItemGuardar);
 		jmnuArchivo.add(jmItemSalir);
+		
+		jmnuEdicion = new JMenu("Edicion");
+		jmbarBarraDeMenus.add(jmnuEdicion);
+		
+		jmItemCortar = new JMenuItem("Cortar");
+		jmItemCortar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_MASK));
+		jmnuEdicion.add(jmItemCortar);
+		
+		jmItemCopiar = new JMenuItem("Copiar");
+		jmItemCopiar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_MASK));
+		jmnuEdicion.add(jmItemCopiar);
+		
+		jmItemPegar = new JMenuItem("Pegar");
+		jmItemPegar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_MASK));
+		jmItemPegar.setMnemonic('P');
+		jmnuEdicion.add(jmItemPegar);
+		
+		jmnuOpciones = new JMenu("Opciones");
+		jmbarBarraDeMenus.add(jmnuOpciones);
+		
+		jmnuFuente = new JMenu("Fuente");
+		jmnuFuente.setMnemonic('F');
+		jmnuOpciones.add(jmnuFuente);
+		
+		jmItemCourierNew = new JMenuItem("Courier New");
+		jmnuFuente.add(jmItemCourierNew);
+		
+		jmItemArial = new JMenuItem("Arial");
+		jmnuFuente.add(jmItemArial);
+		
+		jmItemFPredeterminada = new JMenuItem("Predeterminada");
+		jmnuFuente.add(jmItemFPredeterminada);
+		
+		jmnuTamaño = new JMenu("Tama\u00F1o");
+		jmnuTamaño.setMnemonic('T');
+		jmnuOpciones.add(jmnuTamaño);
+		
+		jmItem16 = new JMenuItem("16");
+		jmnuTamaño.add(jmItem16);
+		
+		jmItem24 = new JMenuItem("24");
+		jmnuTamaño.add(jmItem24);
+		
+		jmItemTPredeterminado = new JMenuItem("Predeterminado");
+		jmnuTamaño.add(jmItemTPredeterminado);
 	}
 	
 	//----------COMMON LISTENERS
@@ -191,17 +282,40 @@ public class TextEditorMain implements ClipboardOwner{
 		@Override
 		public void stateChanged(ChangeEvent e) {
 			Object eventItem = e.getSource();
-			if(eventItem == jmItemAbrir || eventItem == jbtbarAbrir){
-				jmItemAbrirStateChanged(e);
-			}else if(eventItem == jmnuGuardar || eventItem == jbtbarGuardar){
-				jmItemGuardarStateChanged(e);
-			}else if(eventItem == jmItemSalir){
+			
+			if(eventItem == jmItemSalir){
 				jmItemSalirStateChanged(e);
 			}			
 		}
 	};	
+	
+	ChangeListener toolBarItemChangeListener = new ChangeListener() {		
+		@Override
+		public void stateChanged(ChangeEvent e) {
+			Object eventItem = e.getSource();
+			
+			if(eventItem == jbtbarAbrir){				
+				jmItemAbrirStateChanged(e);
+			}
+			if(eventItem == jbtbarGuardar){
+				jmItemGuardarStateChanged(e);
+			}			
+			if(eventItem == jbtbarCortar){
+				jmItemCortarStateChanged(e);
+			}			
+			if(eventItem == jbtbarCopiar){
+				jmItemCopiarStateChanged(e);
+			}			
+			if(eventItem == jbtnbarPegar){
+				jmItemPegarStateChanged(e);
+			}
+		}
+	};	
 
-	//-----------EVENT HANDLERS
+
+
+	//*****************************************EVENT HANDLERS**********
+	//*****************************************************************
 	
 	//Menu Archivo Salir
 	private void jmItemSalirActionPerformed(ActionEvent evt){
@@ -219,7 +333,7 @@ public class TextEditorMain implements ClipboardOwner{
 		jtxtaEditor.requestFocus();
 	}	
 	
-	//Cambios en la barra de estado al cuando cambia el estado del menu salir
+	//**************************LISTERNERs cambio estado items menu
 	private void jmItemSalirStateChanged(ChangeEvent evt){		
 		if(jetbarestPpal.getText().equals("Listo")){			
 			jetbarestPpal.setText("Cierra la aplicacion");
@@ -228,12 +342,13 @@ public class TextEditorMain implements ClipboardOwner{
 		}
 	}
 	
+	//**************************LISTENERs cambio estado items toolbar
 	private void jmItemAbrirStateChanged(ChangeEvent evt){		
 		if(jetbarestPpal.getText().equals("Listo")){			
 			jetbarestPpal.setText("Abrir un fichero");
 		}else{			
 			jetbarestPpal.setText("Listo");
-		}		
+		}
 	}
 	
 	private void jmItemGuardarStateChanged(ChangeEvent evt){		
@@ -244,35 +359,29 @@ public class TextEditorMain implements ClipboardOwner{
 		}
 	}
 	
-	
-	//**********OTHER FUNCTIONS************
-	
-	//Sobrescribe clase Clipboard
-	@Override
-	public void lostOwnership(Clipboard clipboard, Transferable contents) {
-	}
-	
-	//Configurar el portapapeles para copiar y pegar
-	private void copiar(ActionEvent evt){
-		String textToCopy = jtxtaEditor.getSelectedText();
-		Transferable objtrans = new StringSelection(textToCopy);
-		portapapeles.setContents(objtrans, this);
-	}
-	
-	private void pegar(ActionEvent evt){
-		Transferable objTrans = portapapeles.getContents(this);
-		if(portapapeles != null){
-			try {
-				String textToPaste;
-				textToPaste = (String) objTrans.getTransferData(DataFlavor.stringFlavor);
-				jtxtaEditor.replaceSelection(textToPaste);
-			} catch (Exception e) {			
-				e.printStackTrace();
-				System.out.println("EXCEPCION IGNORADA");
-			}
+	private void jmItemCortarStateChanged(ChangeEvent evt){		
+		if(jetbarestPpal.getText().equals("Listo")){			
+			jetbarestPpal.setText("Copia y elimina el texto seleccionado");
+		}else{			
+			jetbarestPpal.setText("Listo");
 		}
 	}
 	
+	private void jmItemCopiarStateChanged(ChangeEvent evt){		
+		if(jetbarestPpal.getText().equals("Listo")){			
+			jetbarestPpal.setText("Copia el texto seleccionado");
+		}else{			
+			jetbarestPpal.setText("Listo");
+		}
+	}
 	
+	private void jmItemPegarStateChanged(ChangeEvent evt){		
+		if(jetbarestPpal.getText().equals("Listo")){			
+			jetbarestPpal.setText("Pega el texto del portapapeles");
+		}else{			
+			jetbarestPpal.setText("Listo");
+		}		
+	}
+
 	
 }
